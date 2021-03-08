5a2f68a464793a235202cf3b5c846ca14ddd2ab2
Marco Domenico Cirillo <59966344+mdciri@users.noreply.github.com>
2021-03-08 15:29:06 +0100

    Update models.py

--- models.py
+++ models.py
@@ -111,6 +111,6 @@ def ensembler():
 
     start = Input((128,128,128,40))
     x = Conv3D(64, kernel_size=3, kernel_initializer='he_normal', padding='same')(start)
-    fin = Conv3D(tar_shape[-1], kernel_size=3, kernel_initializer='he_normal', padding='same', activation='softmax')(start)
+    fin = Conv3D(4, kernel_size=3, kernel_initializer='he_normal', padding='same', activation='softmax')(start)
 
     return Model(inputs=start, outputs=fin, name='Ensembler')
